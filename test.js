d4a4022c8f15af02087bbb8d7f4a74d42e3920c7
LatxTechnologies <latxtechnologies@gmail.com>
2022-07-05 09:02:08 +0530

    date : 5 July

--- test.js
+++ test.js
@@ -1,8 +1,51 @@
-let a =10;
-console.log("outside function ", a);
-if(1)
+// loop : it run same code again and again untill the condition not false
+
+// for
+// while
+
+
+// syntax of for loop
+// for(statement ; condition , increment/ Decrement)
+// {
+//     statement1 
+// }
+
+// for(let i = 1; i<=5 ; i++)
+// {
+//     console.log("Hello" + i)
+// }
+// console.log("message from outside")
+
+
+// While Loop
+
+// let i=1;
+
+// while(i<=3)
+// {
+//     console.log("Hello " + i)
+//     i++;
+// }
+
+
+// break and continue
+
+for(let  i = 1 ; true ; i++)
 {
-    let a = 20; 
-    console.log("Inside Function ", a);  
+    if(i==16)
+    {
+        break;
+    }
+    console.log("Hello " + i)
+
 }
-console.log("after condition check ",a);
+
+
+
+if, else-if, if-else-if , switch
+
+for , while , do-while
+
+break, continue , goto
+
+
